3a6bb0a82a83256b350c18844b78d4491fe6e684
Mike Goren <mike@mortalwombat.net>
2018-01-17 11:09:36 -0800

    update tracks

--- config/initializers/tracks.rb
+++ config/initializers/tracks.rb
@@ -1,7 +1,6 @@
 CURRENT_TRACKS = [
-  '2018 Portland January 2 - July 6: Ruby/Rails track',
-  '2018 Portland January 2 - July 6: C#/.NET track',
-  '2018 Portland January 3 - April 11: Part-time, evening Intro to Programming',
-  '2018 Seattle January 2 - July 6: C#/React track',
-  '2018 Online January 2 - April 12: Part-time, evening Intro to Programming'
+  '2018 Portland March 12 - September 14: CSS/React track',
+  '2018 Portland March 12 - September 14: Java/Android track',
+  '2018 Portland April 16 - July 25: Part-time, evening Intro to Programming',
+  '2018 Seattle March 12 - September 14: C#/React track'
 ]
