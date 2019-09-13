3ade32a39d9add697f305119ee3aef33ee0c466a
Mike Goren <mike@mortalwombat.net>
2019-09-12 17:00:42 -0700

    add timeout so less flaky

--- spec/features/form_filler_spec.rb
+++ spec/features/form_filler_spec.rb
@@ -19,10 +19,15 @@ describe 'form filling' do
         select location, from: "Field254"
         select track, from: location_fields[location]
         find('#Field774_0').set(true) # over 18
+        sleep 1
         find('#Field775_0').set(true) # diploma
+        sleep 1
         find('#Field877_0').set(true) # work eligibility (usa)
+        sleep 1
         find('#Field1321_0').set(true) # work elibility (job search country)
+        sleep 1
         find('#Field1083_0').set(true) # heard about epicodus
+        sleep 1
         click_button 'Sign up!'
       end
       sleep 5
